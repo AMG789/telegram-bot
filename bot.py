29b16a14ea24963aeded051a4f6ae450e8b24a58
AMG <aswany177@gmail.com>
2026-02-13 07:53:41 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
 # ================== Configure Gemini ==================
 genai.configure(api_key=GEMINI_API_KEY)
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-1.5-flash-latest")
 
 # ================== Flask ==================
 app = Flask(__name__)
